08f52f1ccd7010b64c5912a79c09de2447ce512d
apocalypse575 <apocalypse575@gmail.com>
2017-12-15 17:14:10 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+
+  i = 0
+  while i < collection.length
+  yield(collection[i])
+    i += 1
+  end
+    collection
+
+  else
+  puts "Hey! No block was given!"
+  end
 end
